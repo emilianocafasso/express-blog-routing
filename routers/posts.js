271ed58e02dec9d0396eb391fe6fd1ee8cb46b99
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-26 20:41:37 +0100

    Add CRUD routes for posts in the posts router

---
+++ routers/posts.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+
+// index
+router.get('/', function (req, res) {
+    res.send('Lista dei post');
+});
+// show
+router.get('/:id', function (req, res) {
+    res.send('Dettagli del post' + req.params.id);
+});
+// store
+router.post('/', function (req, res) {
+    res.send('Creazione nuovo post');
+});
+// update
+router.put('/posts/:id', function (req, res) {
+    res.send('Modifica integrale del post' + req.params.id);
+});
+// modify
+router.patch('/posts/:id', function (req, res) {
+    res.send('Modifica parziale del post' + req.params.id);
+});
+// destroy
+router.delete('/posts/:id', function (req, res) {
+    res.send('Eliminazione del post' + req.params.id);
+});
+module.exports = router;
